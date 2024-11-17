b2878a8855c4cd7fa8632562a80d21fb9e4a0938
Juan Marcos Cruz Melara <melaratechlab@gmail.com>
2024-11-17 10:45:34 -0600

    App base added

---
+++ TaskManagerApp/TaskManagerApp/TaskManagerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskManagerAppApp.swift
+//  TaskManagerApp
+//
+//  Created by Juan Cruz on 17/11/24.
+//
+
+import SwiftUI
+
+@main
+struct TaskManagerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TaskManagerApp/TaskManagerAppTests/TaskManagerAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  TaskManagerAppTests.swift
+//  TaskManagerAppTests
+//
+//  Created by Juan Cruz on 17/11/24.
+//
+
+import Testing
+@testable import TaskManagerApp
+
+struct TaskManagerAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
